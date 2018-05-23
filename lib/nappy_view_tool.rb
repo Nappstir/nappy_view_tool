3ab6c132b1268dca608d14cd92e7f1a55e643ce6
Siebenhaar <TSiebenhaar@NBK0021031SiebenhaarTravis.local>
2018-05-23 14:58:51 -0600

    NappyTool initial commit.

---
+++ lib/nappy_view_tool.rb
@@ -0,0 +1,6 @@
+require "nappy_view_tool/version"
+require "nappy_view_tool/renderer"
+
+module NappyViewTool
+  # Your code goes here...
+end
---
+++ lib/nappy_view_tool/version.rb
@@ -0,0 +1,3 @@
+module NappyViewTool
+  VERSION = "0.1.0"
+end
